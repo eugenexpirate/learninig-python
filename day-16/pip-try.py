b02ab88f3996cc335dc4c9ab11dd9d8bd71df966
Eugene Nesterenko <xpirate.mail@gmail.com>
2022-07-18 22:31:46 +0200

    change parser to beautifulsoup

--- day-16/pip-try.py
+++ day-16/pip-try.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from prettytable import PrettyTable
 import requests
 from lxml import etree
@@ -11,35 +12,33 @@ import lxml.html
 url = "https://pokemondb.net/pokedex/game/x-y"
 
 
+# api = requests.get(url)
+# tree = lxml.html.document_fromstring(api.text)
+
+# text = tree.text_content()
+# text_list = list(text.splitlines())
+
+name_list, type_list, link_list = [], [], []
+
 api = requests.get(url)
-tree = lxml.html.document_fromstring(api.text)
+content = api.text
 
-text = tree.text_content()
-text_list = list(text.splitlines())
+soup = BeautifulSoup(content, 'lxml')
 
-name_list, type_list = [], []
-
-for element in text_list:
-    if '#0' in element or '#1' in element or '#2' in element:
-        words = element.split()
-        l = len(words)
-        if l == 3:
-            type_list.append(words[2])
-        elif l == 5:
-            type_list.append(words[2] + " " + words[4])
-        elif l == 6:
-            type_list.append(words[3] + " " + words[5])
-            name_list.append(words[1] + " " + words[2])
-            continue
-        
-        name_list.append(words[1])
-
-print(len(name_list))
-print(len(type_list))
-
+location_box = soup.find('div', class_="infocard-list infocard-list-pkmn-lg")
+infocards = location_box.find_all('div', class_ = "infocard")
+for card in infocards:
+    name_list.append(card.find('a', class_='ent-name').get_text())
+    type_text = ""
+    types = card.find_all('small')[1].find_all('a')
+    for type in types:
+        type_text += type.get_text() + " "
+    type_list.append(type_text.strip())
+    link_list.append("https://pokemondb.net" + card.find('a', href=True)['href'])
 
 table = PrettyTable()
 table.add_column("Name", name_list)
 table.add_column("Type", type_list)
+table.add_column("Link", link_list)
 print(table)
 
